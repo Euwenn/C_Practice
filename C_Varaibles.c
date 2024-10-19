c02b8498a9c13b2309163656a5d25f680ff19121
Euwen Meredith <euwenmeredith@outlook.com>
2024-10-18 18:42:20 -0700

    First Commit

---
+++ C_Varaibles.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+  int speed= 20;
+  int time = 7; 
+
+  int distance = speed * time;
+
+  printf("dist: %d\n", distance);
+  
+  return 0;
+}
