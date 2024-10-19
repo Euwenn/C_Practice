4e0c1a07eeda0bd2056eea671ee8d1fe7de7e94f
Euwen Meredith <euwenmeredith@outlook.com>
2024-10-18 23:09:14 -0700

    test

--- C_Varaibles.c
+++ C_Varaibles.c
@@ -3,17 +3,29 @@
 
 int main(void) {
 
-  int speed= 0;
-  int time = 0; 
+  //I'm just playing with variables and testing out some C stuff.
+  /* int speed= 20;
+  int time = 15; 
 
   printf("Speed: ");
-  scanf("%d", &speed);
-  printf("Time:");
-  scanf("%d", &time);
+  printf("Time:"); 
 
   int distance = speed * time;
+  printf("dist: %d\n", distance); */
 
-  printf("dist: %d\n", distance);
+  float x = 5.2;
+  double y = -9.8;
+  char test = "test";
+
+  printf("%c", test);
+
+  printf("x:");
+  scanf("%f", &x);
+  printf("x: %f\n", x);
+  
+
+
+  
 
 
   return 0;
